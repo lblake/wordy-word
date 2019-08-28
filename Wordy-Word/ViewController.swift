06296d12eba4ba90b6ce0b4de684e8eede00810e
Lloyd Blake <treleven.lloyd@gmail.com>
2019-08-28 21:12:10 +0100

    first commit

---
+++ Wordy-Word/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Wordy-Word
+//
+//  Created by Lloyd Blake on 28/08/2019.
+//  Copyright © 2019 Lloyd Blake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+
+            }
+        }
+        
+        if allWords.isEmpty{
+            allWords = ["silkword"]
+        }
+      startGame()
+    }
+    
+    func startGame(){
+        navigationItem.title = allWords.randomElement()
+        //title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+
+}
+
